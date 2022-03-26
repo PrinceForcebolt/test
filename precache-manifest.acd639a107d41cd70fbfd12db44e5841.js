79355486f9c45eebfdd204bef5eb12f2b4bf5092
Prince <100284890+PrinceForcebolt@users.noreply.github.com>
2022-03-26 15:23:48 +0800

    Updates

---
+++ precache-manifest.acd639a107d41cd70fbfd12db44e5841.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94d65e5361ebde8eb07baa06c3528a76",
+    "url": "/test/index.html"
+  },
+  {
+    "revision": "2317bfc0b98821660178",
+    "url": "/test/static/css/main.a7a74fcd.chunk.css"
+  },
+  {
+    "revision": "9cbe13e10c9f113485c1",
+    "url": "/test/static/js/2.e96df388.chunk.js"
+  },
+  {
+    "revision": "2317bfc0b98821660178",
+    "url": "/test/static/js/main.a6334209.chunk.js"
+  },
+  {
+    "revision": "41261cfbee06faf3d8ed",
+    "url": "/test/static/js/runtime-main.1fe701f0.js"
+  }
+]);
